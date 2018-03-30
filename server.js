243f344749c2f46cf82d3b9758df98e6d9f007f4
Schonkar_miracle <schonkar@miraclesoft.com>
2018-03-30 09:03:04 -0400

    init server commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+var port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/'));
+
+//Enabling CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+require('./app/routes/tokenAuth.js')(app);
+
+app.get('/', function(req, res){
+  res.send("Okay");
+})
+
+app.listen(port);
+console.log("Started listening on : " + port);
